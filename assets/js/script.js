14c7d37d49c3d37a11e4715183fc29e158c56a21
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-04-14 17:07:17 +0000

    linter error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-const mobileBtn = document.getElementById("mobile-nav");
-const mobileMenu = document.getElementById("mobile-menu");
+const mobileBtn = document.getElementById('mobile-nav');
+const mobileMenu = document.getElementById('mobile-menu');
 const closeBtn = document.getElementById('close-modal');
 
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 
 mobileBtn.onclick = () => {
-  body.style.overflow = "hidden";
-  mobileMenu.style.display = "block";
+  body.style.overflow = 'hidden';
+  mobileMenu.style.display = 'block';
 };
 
 closeBtn.onclick = () => {
-  mobileMenu.style.display = "none";
-  body.style.overflow = "auto";
+  mobileMenu.style.display = 'none';
+  body.style.overflow = 'auto';
 };
